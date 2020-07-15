08c5f65695978607959715e831e5ca816df26ba4
aom <onpreeya.k@rmutsvmail.com>
2020-07-15 14:56:16 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
+        System.out.println("onpreeya kleebkaew");
+        System.out.println("363211760038 ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+        System.out.println("เทคโนโลยีการจัดการ");
+
 
 
 
