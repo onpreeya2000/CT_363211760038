2861d4c048abe1c14f8aedaf1abbdb70422bdcc6
onpreeya <onpreeya.k@rmutsvmail.com>
2020-08-05 15:49:05 +0700

    Chapter4_1

---
+++ src/Lab3/Example_if_eles_if.java
@@ -0,0 +1,42 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Example_if_eles_if {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //input score
+        System.out.println("Enter your score:");
+        double score = scanner.nextDouble();
+        //test condition
+        // F  = 0  - 49.99
+        // D  = 50.00 - 54.99
+        // D+ = 55.00 - 59.99
+        // C  = 60.00 - 64.99
+        // C+ = 65.00 - 69.00
+        // B  = 70.00 - 74.00
+        // B+ = 75.00 - 79.00
+        // A  = 80.00 - 100.00
+        if (score >= 0 && score <= 49.99) {
+            System.out.println("Your grade is F.");
+        } else if (score >= 50.00 && score <= 54.99){
+            System.out.println("Your grade is D.");
+        } else if (score >= 55.00 && score <= 59.99){
+            System.out.println("Your grade is D+.");
+        } else if (score >= 60.00 && score <= 64.99){
+            System.out.println("Your grade is C.");
+        } else if (score >= 65.00 && score <= 69.99){
+            System.out.println("Your grade is C+.");
+        } else if (score >= 70.00 && score <= 74.99){
+            System.out.println("Your grade is B.");
+        } else if (score >= 75.00 && score <= 79.99){
+            System.out.println("Your grade is B+.");
+        } else if (score >= 80.00 && score <= 100.99){
+            System.out.println("Your grade is A.");
+        } else {
+            // coding here ...
+        }
+
+    }
+}
---
+++ src/Lab3/Example_lf.java
@@ -0,0 +1,32 @@
+package Lab3;
+
+public class Example_lf {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 30;
+
+        if (x<y) {
+            System.out.println("x more than y");
+            System.out.println("Hello.");
+        }
+        System.out.println("Good Bye.");
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
