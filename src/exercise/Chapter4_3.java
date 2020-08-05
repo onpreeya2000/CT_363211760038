3c2dba7c1aa330c162795c478938fe3bc712d835
onpreeya <onpreeya.k@rmutsvmail.com>
2020-08-05 14:22:52 +0700

    Chapter4_1

--- src/exercise/Chapter4_3.java
+++ src/exercise/Chapter4_3.java
@@ -3,9 +3,17 @@ package exercise;
 import java.util.Scanner;
 
 public class Chapter4_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
 
+        System.out.println("Enter your aga: ");
+        int x = scanner.nextInt();
 
+        System.out.println("Enter your email: ");
+        String email = scanner.nextLine();
 
 
 
