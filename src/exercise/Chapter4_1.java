97f9f8540a9cca928ea79d3a59d2a5052095085a
onpreeya <onpreeya.k@rmutsvmail.com>
2020-08-05 12:31:22 +0700

    Chapter4_1

--- src/exercise/Chapter4_1.java
+++ src/exercise/Chapter4_1.java
@@ -6,10 +6,26 @@ public class Chapter4_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter you name ?: ");
-        String name = scanner.nextLine();
-
-
+        System.out.println("Enter a score 1 : ");
+        int a =scanner.nextInt();
+        System.out.println("Enter a score 2 : ");
+        int b =scanner.nextInt();
+        System.out.println("Enter a score 3 : ");
+        int c =scanner.nextInt();
+        System.out.println("Enter a score 4 : ");
+        int d =scanner.nextInt();
+        System.out.println("Enter a score 5 : ");
+        int e =scanner.nextInt();
+        System.out.println("Enter a score 6 : ");
+        int f =scanner.nextInt();
+        System.out.println("Enter a score 7 : ");
+        int g =scanner.nextInt();
+        System.out.println("Enter a score 8 : ");
+        int h =scanner.nextInt();
+        System.out.println("Enter a score 9 : ");
+        int i =scanner.nextInt();
+        System.out.println("Enter a score 10 : ");
+        int j =scanner.nextInt();
 
 
 
