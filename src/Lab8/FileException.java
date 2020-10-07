1863bcec9ad6c52f67781d03643b6e4ab2d9facd
onpreeya <onpreeya.k@rmutsvmail.com>
2020-10-07 15:33:53 +0700

    Lab8

---
+++ src/Lab8/FileException.java
@@ -0,0 +1,25 @@
+package Lab8;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileException {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fr = null;
+
+        File file = new File("D:/Myname.txt");
+
+        fr = new FileReader(file);
+        char [] txt = new char[50];
+        fr.read(txt);
+
+        for (char c:txt) {
+            System.out.print(c);
+
+        }
+
+    }
+}
